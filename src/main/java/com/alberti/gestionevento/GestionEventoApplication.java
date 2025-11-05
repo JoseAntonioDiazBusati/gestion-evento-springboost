5a6905a84dfe58d8e0593caac73427f34035d0f2
JoseAntonio <jdiabus511@g.educaand.es>
2025-11-05 08:44:14 +0100

    initial commit

---
+++ src/main/java/com/alberti/gestionevento/GestionEventoApplication.java
@@ -0,0 +1,13 @@
+package com.alberti.gestionevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alberti/gestionevento/GestionEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberti.gestionevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
